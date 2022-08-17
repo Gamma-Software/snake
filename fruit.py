08b41eee914d8f049d4fa9e986ca418d0290b851
Valentin <valentin.rudloff.perso@gmail.com>
2022-08-17 12:51:31 +0200

    use fruit class

--- fruit.py
+++ fruit.py
@@ -1,10 +1,17 @@
 import numpy as np
+import pygame
+import random
+from game_area import GameArea
 
 class Fruit:
-    def __init__(self, _game_area) -> None:
+    def __init__(self, _game_area: GameArea) -> None:
         print("fruit: spawn")
-        self.game_area = _game_area
-        self.position = (np.rand(self.game_area.min_x, self.game_area.max_x), np.rand(self.game_area.min_y, self.game_area.max_y))
-    
+        self.position = [
+            np.linspace(0, _game_area.width, num=_game_area.width//5, endpoint=False, dtype=int)[random.randint(0, _game_area.width//5-1)],
+            np.linspace(0, _game_area.width, num=_game_area.height//5, endpoint=False, dtype=int)[random.randint(0, _game_area.width//5-1)]]
+
     def __del__(self):
         print("fruit: eaten")
+
+    def draw(self, surface: pygame.Surface) -> None:
+        pygame.draw.rect(surface, pygame.Color(0, 255, 0), pygame.Rect(self.position[0], self.position[1], 5, 5))
